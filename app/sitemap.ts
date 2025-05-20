3828ac572337ac7c0a2297b1a04f7feefa1838d0
heavyduty5073 <heavyduty5073@gmail.com>
2025-05-20 14:27:12 +0900

    SEO 세팅

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,24 +1,24 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
+    const lastModified = new Date();
+
     return [
-        {
-            url: 'https://yourcompany.com',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 1,
-        },
-        {
-            url: 'https://yourcompany.com/business/trip',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8,
-        },
-        {
-            url: 'https://yourcompany.com/business/parts',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8,
-        },
+        { url: 'https://yourcompany.com', lastModified, changeFrequency: 'monthly', priority: 1.0 },
+        { url: 'https://yourcompany.com/introduce', lastModified, changeFrequency: 'monthly', priority: 0.7 },
+        { url: 'https://yourcompany.com/introduce/about', lastModified, changeFrequency: 'monthly', priority: 0.7 },
+
+        { url: 'https://yourcompany.com/business/construction', lastModified, changeFrequency: 'monthly', priority: 0.8 },
+        { url: 'https://yourcompany.com/business/parts', lastModified, changeFrequency: 'monthly', priority: 0.8 },
+        { url: 'https://yourcompany.com/business/repair', lastModified, changeFrequency: 'monthly', priority: 0.8 },
+        { url: 'https://yourcompany.com/business/trip', lastModified, changeFrequency: 'monthly', priority: 0.8 },
+
+        { url: 'https://yourcompany.com/parts', lastModified, changeFrequency: 'monthly', priority: 0.7 },
+        { url: 'https://yourcompany.com/repair', lastModified, changeFrequency: 'monthly', priority: 0.7 },
+
+        { url: 'https://yourcompany.com/support', lastModified, changeFrequency: 'monthly', priority: 0.6 },
+        { url: 'https://yourcompany.com/support/write', lastModified, changeFrequency: 'monthly', priority: 0.6 },
+
+        { url: 'https://yourcompany.com/login', lastModified, changeFrequency: 'yearly', priority: 0.3 },
     ];
 }
