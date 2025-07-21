d6f8ebda00fde62739f987df9875ee9071fcc433
heavyduty5073 <heavyduty5073@gmail.com>
2025-07-21 11:12:51 +0900

    구글 아날리틱스 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import Script from "next/script";
+import GoogleTagManager from "@/components/analytics/GoogleTagManager";
 // 폰트 설정
 const inter = Inter({
     subsets: ["latin"],
@@ -493,6 +494,7 @@ export default function RootLayout({
             </Script>
         </head>
         <body className="antialiased">
+        <GoogleTagManager/>
         {children}
         <Analytics />
         <SpeedInsights />
